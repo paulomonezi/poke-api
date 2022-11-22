0e7e215b46137f1a884711ce5a63aa61eca86da0
Paulo Monezi <paulo.monezi@icloud.com>
2022-11-22 19:17:52 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Card } from './components/card';
+import { baseUrl, initialUrl } from './poke-api/poke-api'
+
+function App() {
+  return (
+    <div>
+      <Card>
+      </Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Card = () => {
+    return (
+        <>
+            <div>Card</div>
+            <div>Card</div>
+            <div>Card</div>
+        </>
+    )
+}
