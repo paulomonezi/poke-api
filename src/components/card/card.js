e913752880ceee2109feffd0b9d67a318110066b
Paulo Monezi <paulo.monezi@icloud.com>
2022-11-26 03:04:49 -0400

    feat: implemented basic pokemon card

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,17 +1,14 @@
 import React from 'react'
+import { GetPokemonData } from '../../services/pokeapi'
 import * as C from './styles'
-import { getImage } from '../../services/poke-api/poke-api'
 
 export const Card = () => {
     return (
         <>
-            <C.PokemonCard>
-
-            </C.PokemonCard>
-            {getImage()}
-            <div>Card</div>
-            <div>Card</div>
-            <div>Card</div>
+        <C.PokemonCard>
+        <GetPokemonData></GetPokemonData>
+        </C.PokemonCard>
+        
         </>
     )
 }
