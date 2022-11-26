a05085f88de0e7f8a26ffcd10235a88d2f4fd3d0
Paulo Monezi <paulo.monezi@icloud.com>
2022-11-26 03:04:49 -0400

    chore: implemented basic pokemon card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import { Card } from './components/card/card';
-import { baseUrl, initialUrl } from './services/poke-api/poke-api'
 
 function App() {
   return (
     <div>
-      <Card>
-      </Card>
+      <Card></Card>
     </div>
   );
 }
---
+++ src/services/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const CardWrapper = styled.div`
+height: 300px;
+width: 200px;
+border-radius: 10px;
+background-color: teal;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+`
+
+export const Sprite = styled.img`
+`
+
