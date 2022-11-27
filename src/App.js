c0c342680aabb3c6dc4ceb4b19396a5ae5a707c8
Paulo Monezi <paulo.monezi@icloud.com>
2022-11-27 00:47:25 -0400

    feat: add a global style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { Card } from './components/card/card';
+import { GlobalStyle } from './services/css/reset';
 
 function App() {
   return (
     <div>
       <Card></Card>
+      <GlobalStyle/>
     </div>
   );
 }
 
 export default App;
+
+
+
