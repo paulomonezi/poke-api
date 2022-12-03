bcbebb754ca89bad23d3385ee1c43ebeaa800986
Paulo Monezi <paulo.monezi@icloud.com>
2022-12-03 07:38:59 -0400

    style: implemented MainContainer to wrap the home list

--- src/components/card/styles.js
+++ src/components/card/styles.js
@@ -1,7 +1,18 @@
 import styled from 'styled-components'
 
 export const CardWrapper = styled.div`
-max-height: 200px;
+max-height: 265px;
 max-width: 200px;
 color: black;
+`
+
+export const MainContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-wrap: wrap;
+gap: 8px;
+margin: 10px 0px 0px 0px;
+width: 100%;
+height: auto;
 `
