913c1d7c45a4c4ef21c30612e4396ae3deb2e373
Paulo Monezi <paulo.monezi@icloud.com>
2022-12-06 00:07:10 -0400

    feat(details page): implemented using react router

--- src/pages/details.js
+++ src/pages/details.js
@@ -1,5 +1,10 @@
+import { PokemonDetails } from "../services/pokeapi"
+import { useParams } from "react-router-dom"
+
 const Details = () => {
-    return <h1>Details</h1>
+   const { name } = useParams()
+
+   return <PokemonDetails pokeDetailsName={name} />
 }
 
 export { Details }
