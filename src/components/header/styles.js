dcf91505847b0fa2688a494c68414f8dfe21ebf9
Paulo Monezi <paulo.monezi@icloud.com>
2022-12-09 10:03:38 -0400

    fix(header): fixed missing file

---
+++ src/components/header/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+export const Header = styled.div`
+width: 80%;
+min-height: 270px;
+border-bottom: 1px solid black;
+margin-bottom: 50px;
+`
