1a5304df626e571bab2fe9d1305f52cd6c4bea69
Paulo Monezi <paulo.monezi@icloud.com>
2022-12-11 21:06:42 -0400

    feat: implemented a separator component for sections

---
+++ src/components/separator/separator.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SeparatorLine } from './styles'
+
+export const Separator = () => {
+    return (
+        <SeparatorLine>
+           <div>  </div>
+        </SeparatorLine>
+    )
+}
---
+++ src/components/separator/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const SeparatorLine = styled.span`
+border-bottom: 1px solid red;
+`
